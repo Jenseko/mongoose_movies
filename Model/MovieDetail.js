3d3ff2b82a609ffc57fc537f8438385bb2eed749
Jenseko <jenscode23@gmail.com>
2023-06-02 15:05:04 +0200

    lvl_1-2 finished

---
+++ Model/MovieDetail.js
@@ -0,0 +1,115 @@
+import { model, Schema } from "mongoose";
+
+const movieDetailsSchema = new Schema(
+    {
+        "title": {
+            "type": "String"
+        },
+        "year": {
+            "type": "Number"
+        },
+        "rated": {
+            "type": "String"
+        },
+        "runtime": {
+            "type": "Number"
+        },
+        "countries": {
+            "type": [
+                "String"
+            ]
+        },
+        "genres": {
+            "type": [
+                "String"
+            ]
+        },
+        "director": {
+            "type": "String"
+        },
+        "writers": {
+            "type": [
+                "String"
+            ]
+        },
+        "actors": {
+            "type": [
+                "String"
+            ]
+        },
+        "plot": {
+            "type": "String"
+        },
+        "poster": {
+            "type": "String"
+        },
+        "imdb": {
+            "id": {
+                "type": "String"
+            },
+            "rating": {
+                "type": "Number"
+            },
+            "votes": {
+                "type": "Number"
+            }
+        },
+        "tomato": {
+            "meter": {
+                "type": "Number"
+            },
+            "image": {
+                "type": "String"
+            },
+            "rating": {
+                "type": "Number"
+            },
+            "reviews": {
+                "type": "Number"
+            },
+            "fresh": {
+                "type": "Number"
+            },
+            "consensus": {
+                "type": "String"
+            },
+            "userMeter": {
+                "type": "Number"
+            },
+            "userRating": {
+                "type": "Number"
+            },
+            "userReviews": {
+                "type": "Number"
+            }
+        },
+        "metacritic": {
+            "type": "Number"
+        },
+        "awards": {
+            "wins": {
+                "type": "Number"
+            },
+            "nominations": {
+                "type": "Number"
+            },
+            "text": {
+                "type": "String"
+            }
+        },
+        "type": {
+            "type": "String"
+        }
+    }
+    ,
+    { collection: "movieDetails" });
+
+
+// Mongoose erstellt automatisch collection Namen
+// MovieDetail --> moviedetails
+
+// wenn wir im Schema ein zweites object (options) 
+// übergeben können wir den Collection Name angeben
+const MovieDetail = model("MovieDetail", movieDetailsSchema);
+
+export default MovieDetail;
---
+++ movieDetailsExercise.js
@@ -0,0 +1,126 @@
+import mongoose from "mongoose";
+import MovieDetail from "./mongoose-first-steps/Models/MovieDetail.js";
+
+
+mongoose.connect("mongodb://127.0.0.1:27017/video")
+// .then((connection) => console.log("Connected"));
+
+
+// -------lvl_1-2_quering-mit-mongoose -------------
+
+
+// ----------------- 1 ----------------------------- 
+
+
+const movies1 = await MovieDetail.find(
+    { director: "Steven Spielberg" },
+    { titel: 1 },
+    null);
+
+console.log(movies1);
+
+
+// ----------------- 2 ----------------------------- 
+
+
+const movies2 = await MovieDetail.find(
+    { "tomato.userReviews": { $gt: 40_000 } },
+    null,
+    { "tomato.userReviews": -1 },
+    { limit: 20 });
+
+console.log(movies2);
+
+
+// ----------------- 3 ----------------------------- 
+
+
+const movies3 = await MovieDetail.find(
+    { "year": { $gte: 2000, $lte: 2005 } },
+    { title: 1, year: 1 },
+    {});
+
+console.log(movies3);
+
+
+// ----------------- 4 ----------------------------- 
+
+
+const movies4 = await MovieDetail.find(
+    { "tomato.userRating": { $gte: 4.0 }, year: { $gt: 2010 } },
+    { title: 1, director: 1 },
+    {});
+
+console.log(movies4);
+
+
+// ----------------- 5 ----------------------------- 
+
+
+const movies5 = await MovieDetail.find(
+    { "tomato.userReviews": { $le: 1_000 }, year: { $lt: 2005 } },
+    null,
+    { "tomato.userReviews": 1 },
+    { limit: 10 });
+
+console.log(movies5);
+
+
+// ----------------- 6 ----------------------------- 
+
+
+const movies6 = await MovieDetail.find(
+    { tomato: { $exists: false } },
+    null,
+    null);
+
+console.log(movies6);
+
+
+// ----------------- 7 ----------------------------- 
+
+
+const movies7 = await MovieDetail.find(
+    { "imdb.votes": { $lt: 1_000, $gt: 100 } },
+    { title: 1, "imdb.rating": 1 },
+    null);
+
+console.log(movies7);
+
+
+// ----------------- 8 ----------------------------- 
+
+
+const movies8 = await MovieDetail.find(
+    null,
+    null,
+    { "imdb.rating": -1 });
+
+console.log(movies8);
+
+
+// ----------------- 9 ----------------------------- 
+
+
+const movies9 = await MovieDetail.find(
+    null,
+    null,
+    { "imdb.rating": -1 },
+    { limit: 10 });
+
+console.log(movies9);
+
+
+// ----------------- 10 ----------------------------- 
+
+
+const movies10 = await MovieDetail.find(
+    { genres: ["Crime", "Drama"] },
+    { title: 1, genres: 1 },
+    { "imdb.rating": 1 },
+);
+
+console.log(movies10);
+
+
+// --------------------------------------------------
